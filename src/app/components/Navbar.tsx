a4c9c985d2e4199846da6d37561af171d9d0fd5e
favil <fatimavillena@gmail.com>
2024-08-25 00:07:46 -0400

    [FIX] hide navbar when not on upload or view

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import './Navbar.css';
 
 const Navbar: React.FC = () => {
+  const pathname = usePathname();
+
+  const showNavbar = pathname === '/upload' || pathname === '/view';
+
+  if (!showNavbar) {
+    return null;
+  }
+
   return (
     <nav className="navbar">
       <Link className="navbar-link" href="/upload">
